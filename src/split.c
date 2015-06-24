09b970f7a5be46f2b8dc94478cd1b633aa36c969
AdamSimpson <adambsimpson@gmail.com>
2015-06-24 12:37:32 -0400

    Better ability to call underlying MPI_Init/Finalize

--- src/split.c
+++ src/split.c
@@ -131,19 +131,19 @@ void SplitInit() {
 
 int MPI_Init(int *argc, char ***argv) {
   // Allow MPI_Init to be called directly
-  // It is assumed the user calls SplitInit() immediately after MPI_Init
-  // This was added for Cray may do something special in MPI_Init
+  // This was added as Cray may do something special in MPI_Init
+  int return_value;
   if (getenv("W_UNWRAP_INIT")) {
     DEBUG_PRINT("Unwrapped!\n");
     int (*real_MPI_Init)(int*, char***) = dlsym(RTLD_NEXT, "MPI_Init");
-    int return_value = (*real_MPI_Init)(argc, argv);
-    return return_value;
+    return_value = (*real_MPI_Init)(argc, argv);
+  }
+  else {
+    DEBUG_PRINT("Wrapped!\n");
+    return_value = PMPI_Init(argc, argv);
   }
 
-  DEBUG_PRINT("Wrapped!\n");
-  int return_value = PMPI_Init(argc, argv);
   SplitInit();
-
   return return_value;
 }
 
@@ -154,7 +154,18 @@ int MPI_Finalize() {
   if(err != MPI_SUCCESS)
     EXIT_PRINT("Failed to free split communicator: %d !\n", err);
 
-  return PMPI_Finalize();
+  int return_value;
+  if (getenv("W_UNWRAP_FINALIZE")) {
+    DEBUG_PRINT("Unwrapped!\n");
+    int (*real_MPI_Finalize)() = dlsym(RTLD_NEXT, "MPI_Finalize");
+    return_value = (*real_MPI_Finalize)();
+  }
+  else {
+    DEBUG_PRINT("Wrapped!\n");
+    return_value = PMPI_Finalize();
+  }
+
+  return return_value;
 }
 
 // If input_comm == MPI_COMM_WORLD return MPI_COMM_SPLIT else input_comm
