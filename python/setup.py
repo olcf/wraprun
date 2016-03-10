b4fd48ccea01cfd4c9715ee0ce998a1da08e3cf1
Matt Belhorn <belhornmp@ornl.gov>
2016-03-10 13:45:53 -0500

    v0.2.1 pre-release install fixes.

--- python/setup.py
+++ python/setup.py
@@ -8,12 +8,13 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
 
     setup(name='Wraprun',
-          version='0.0.1',
+          version='0.2.1',
           description='Flexible Aprun task bundler',
           author='Matt Belhorn',
           author_email='belhornmp@ornl.gov',
           url='https://github.com/olcf/wraprun',
           license="MIT",
           packages=find_packages(),
+          setup_requires=['pyyaml>=3.1.0'],
           install_requires=['pyyaml>=3.1.0'],
           scripts=['bin/wraprun'])
