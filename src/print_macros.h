6fb32ca6a6adcd8b760a5c247fd9f340834c7f24
AdamSimpson <adambsimpson@gmail.com>
2016-04-18 14:41:17 -0400

    printf hex modification

--- src/print_macros.h
+++ src/print_macros.h
@@ -5,7 +5,7 @@
 #include <errno.h>
 
 #ifdef DEBUG
-#define DEBUG_PRINT(str, args...) do { printf("DEBUG: %s:%d:%s():MPI_COMM_SPLIT=%p : " str, \
+#define DEBUG_PRINT(str, args...) do { printf("DEBUG: %s:%d:%s():MPI_COMM_SPLIT=%04x : " str, \
  __FILE__, __LINE__, __func__, MPI_COMM_SPLIT, ##args); } while(0)
 #else
 #define DEBUG_PRINT(str, args...) do {} while (0)
